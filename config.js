3422620befc1dfc8eb88cf3cda2b496737e1ec3d
aabdoabdou21-pixelu21 <abdougaming212@gmail.com>
2026-09-21 23:49:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // ضع هنا بيانات مشروع Supabase الحقيقي.
 // لا تستخدم service_role key.
 window.SXPAY_CONFIG = {
-  SUPABASE_URL: "PASTE_YOUR_SUPABASE_URL",
-  SUPABASE_ANON_KEY: "PASTE_YOUR_SUPABASE_ANON_KEY"
+  SUPABASE_URL: "https://lfksnblbftqfofzxhujj.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_2PBN7XPAjmdV5PEPMiuEkA_nE452MTe"
 };
